660a5242c6d0aff8d809d4b7f48ea4e28fd5754e
Seb <seebeesbt@gmail.com>
2022-07-18 18:27:38 +0100

    Made howtoxicami work with other users

--- src/main/java/de/voidtech/alison/commands/HowToxicAmICommand.java
+++ src/main/java/de/voidtech/alison/commands/HowToxicAmICommand.java
@@ -12,6 +12,8 @@ import main.java.de.voidtech.alison.service.WordService;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.utils.Result;
 
 @Command
 public class HowToxicAmICommand extends AbstractCommand {
@@ -24,28 +26,42 @@ public class HowToxicAmICommand extends AbstractCommand {
 	
 	@Override
 	public void execute(Message message, List<String> args) {
-		String userID = message.getAuthor().getId();
-		if (privacyService.userIsIgnored(userID)) {
-			message.reply("This user has chosen not to be analysed!").mentionRepliedUser(false).queue();
-			return;
-		}
-		Toxicity howToxic = wordService.scoreUser(userID);
-		if (howToxic == null) {
-			message.reply("I couldn't find any data to analyse!").mentionRepliedUser(false).queue();
-			return;
-		}
-		MessageEmbed toxicityEmbed = new EmbedBuilder()
-				.setColor(getColour(howToxic))
-				.setTitle("How toxic is " + message.getAuthor().getName() + "?")
-				.setDescription("I searched `" + howToxic.getTotalWordCount() +
-						"` words. From this, I found `" + howToxic.getTokenCount() +
-						"` words with meaning.\n" +
-						"**Positive words found: `" + howToxic.getPositives() + "`**\n" +
-						"**Negative words found: `" + howToxic.getNegatives() + "`**\n" +
-						"**Total Score (higher is better!): `" + howToxic.getScore() + "`**")
-				.setFooter(getMessage(howToxic))
-				.build();
-		message.replyEmbeds(toxicityEmbed).mentionRepliedUser(false).queue();
+		
+    	String userID;
+    	if (args.isEmpty()) userID = message.getAuthor().getId();
+    	else userID = args.get(0).replaceAll("([^0-9])", "");
+    	
+        if (userID.equals("")) {
+            message.reply("I couldn't find that user :(").mentionRepliedUser(false).queue();
+            return;
+        }
+        
+        Result<User> userResult = message.getJDA().retrieveUserById(userID).mapToResult().complete();
+        if (userResult.isSuccess()) {
+        	if (privacyService.userIsIgnored(userID)) {
+    			message.reply("This user has chosen not to be analysed!").mentionRepliedUser(false).queue();
+    			return;
+    		}
+    		Toxicity howToxic = wordService.scoreUser(userID);
+    		if (howToxic == null) {
+    			message.reply("I couldn't find any data to analyse!").mentionRepliedUser(false).queue();
+    			return;
+    		}
+    		MessageEmbed toxicityEmbed = new EmbedBuilder()
+    				.setColor(getColour(howToxic))
+    				.setTitle("How toxic is " + userResult.get().getName() + "?")
+    				.setDescription("I searched `" + howToxic.getTotalWordCount() +
+    						"` words. From this, I found `" + howToxic.getTokenCount() +
+    						"` words with meaning.\n" +
+    						"**Positive words found: `" + howToxic.getPositives() + "`**\n" +
+    						"**Negative words found: `" + howToxic.getNegatives() + "`**\n" +
+    						"**Total Score (higher is better!): `" + howToxic.getScore() + "`**")
+    				.setFooter(getMessage(howToxic))
+    				.build();
+    		message.replyEmbeds(toxicityEmbed).mentionRepliedUser(false).queue();
+        } else {
+        	message.reply("I couldn't find that user :(").mentionRepliedUser(false).queue();
+        }
 	}
 
 	private String getMessage(Toxicity howToxic) {
