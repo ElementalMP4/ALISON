b5a50d6aa33034e4a2e0d32953b6b1dc0175a22d
Seb <seebeesbt@gmail.com>
2022-07-20 17:46:22 +0100

    vErSiOn

--- src/main/java/de/voidtech/alison/GlobalConstants.java
+++ src/main/java/de/voidtech/alison/GlobalConstants.java
@@ -4,5 +4,5 @@ public class GlobalConstants
 {
     public static final String STREAM_URL = "https://twitch.tv/elementalmp4";
     public static final String REPO_URL = "https://github.com/Elementalmp4/ALISON";
-    public static final String VERSION = "1.2.5 - Linguistic Latte";
+    public static final String VERSION = "1.2.6 - Linguistic Latte";
 }
